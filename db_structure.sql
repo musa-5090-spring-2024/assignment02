fb7a0da06ce612840f11dbbdd586d29635910834
Emily Zhou <zhou@middlebury.edu>
2024-02-21 22:11:52 -0500

    Update db_structure.sql

--- db_structure.sql
+++ db_structure.sql
@@ -26,28 +26,90 @@ the assignment README.
 Finally, you can run this file either by copying it all into PG Admin, or by
 running the following command from the command line:
 
-    psql -U postgres -d <YOUR_DATABASE_NAME> -f db_structure.sql
+    psql -U postgres -d musa-5090 -f db_structure.sql
 
 */
 
--- Create schema to store all the datasets
-create schema if not exists census;
-create schema if not exists septa;
-create schema if not exists phl;
-create schema if not exists azavea;
 
 -- Preprocess census.population_2020
 -- select only the columns we need, delete unnecessary rows, and change the data types of the columns
-create table census.temp_census as
-select
-    "GEO_ID" as geoid,
-    "NAME" as geoname,
-    "P1_001N" as total
-from census.population_2020;
 
-drop table census.population_2020;
+ALTER TABLE census.population_2020
+DROP COLUMN "P1_002N",
+DROP COLUMN "P1_003N",
+DROP COLUMN "P1_004N",
+DROP COLUMN "P1_005N",
+DROP COLUMN "P1_006N",
+DROP COLUMN "P1_007N",
+DROP COLUMN "P1_008N",
+DROP COLUMN "P1_009N",
+DROP COLUMN "P1_010N",
+DROP COLUMN "P1_011N",
+DROP COLUMN "P1_012N",
+DROP COLUMN "P1_013N",
+DROP COLUMN "P1_014N",
+DROP COLUMN "P1_015N",
+DROP COLUMN "P1_016N",
+DROP COLUMN "P1_017N",
+DROP COLUMN "P1_018N",
+DROP COLUMN "P1_019N",
+DROP COLUMN "P1_020N",
+DROP COLUMN "P1_021N",
+DROP COLUMN "P1_022N",
+DROP COLUMN "P1_023N",
+DROP COLUMN "P1_024N",
+DROP COLUMN "P1_025N",
+DROP COLUMN "P1_026N",
+DROP COLUMN "P1_027N",
+DROP COLUMN "P1_028N",
+DROP COLUMN "P1_029N",
+DROP COLUMN "P1_030N",
+DROP COLUMN "P1_031N",
+DROP COLUMN "P1_032N",
+DROP COLUMN "P1_033N",
+DROP COLUMN "P1_034N",
+DROP COLUMN "P1_035N",
+DROP COLUMN "P1_036N",
+DROP COLUMN "P1_037N",
+DROP COLUMN "P1_038N",
+DROP COLUMN "P1_039N",
+DROP COLUMN "P1_040N",
+DROP COLUMN "P1_041N",
+DROP COLUMN "P1_042N",
+DROP COLUMN "P1_043N",
+DROP COLUMN "P1_044N",
+DROP COLUMN "P1_045N",
+DROP COLUMN "P1_046N",
+DROP COLUMN "P1_047N",
+DROP COLUMN "P1_048N",
+DROP COLUMN "P1_049N",
+DROP COLUMN "P1_050N",
+DROP COLUMN "P1_051N",
+DROP COLUMN "P1_052N",
+DROP COLUMN "P1_053N",
+DROP COLUMN "P1_054N",
+DROP COLUMN "P1_055N",
+DROP COLUMN "P1_056N",
+DROP COLUMN "P1_057N",
+DROP COLUMN "P1_058N",
+DROP COLUMN "P1_059N",
+DROP COLUMN "P1_060N",
+DROP COLUMN "P1_061N",
+DROP COLUMN "P1_062N",
+DROP COLUMN "P1_063N",
+DROP COLUMN "P1_064N",
+DROP COLUMN "P1_065N",
+DROP COLUMN "P1_066N",
+DROP COLUMN "P1_067N",
+DROP COLUMN "P1_068N",
+DROP COLUMN "P1_069N",
+DROP COLUMN "P1_070N",
+DROP COLUMN "P1_071N",
 
-alter table census.temp_census rename to population_2020;
+ALTER TABLE census.population_2020
+RENAME COLUMN "GEO_ID" TO geoid,
+RENAME COLUMN "NAME" TO geoname,
+RENAME COLUMN "P1_001N" TO total;
 
 delete from census.population_2020
 where geoid = 'Geography';
