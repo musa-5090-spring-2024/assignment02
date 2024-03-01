a05f67997de7a408910e03778a2423b88cf9c3c4
Michael Clifford <mclif89@gmail.com>
2024-03-01 11:29:15 -0500

    friday work

--- query08.sql
+++ query08.sql
@@ -18,13 +18,22 @@ Discuss which dataset you chose for defining Penn's campus.
 
 with penn as (
     select
-        *
+        st_union(st_buffer(geog, 50)::geometry) as buff
     from phl.pwd_parcels
-    where ((owner1 like 'TRS%' OR owner1 like 'TRUSTEE%')
+    where ((owner1 like 'TRS%' OR owner1 like '%TRUSTEE%')
         and (owner1 like '%UNIV%' or owner1 like 'PENN')) or
-        ((owner1 like 'TRS%' OR owner1 like 'TRUSTEE%')
+        ((owner1 like 'TRS%' OR owner1 like '%TRUSTEE%')
         and (owner2 like '%UNIV%' or owner2 like 'PENN')) or
-        ((owner2 like 'TRS%' OR owner2 like 'TRUSTEE%')
-        and (owner2 like '%UNIV%' or owner2 like 'PENN'))
+        ((owner2 like 'TRS%' OR owner2 like '%TRUSTEE%')
+        and (owner2 like '%UNIV%' or owner2 like 'PENN')) or
+		owner1 like 'WISTAR INSTITUTE OF ANATO' or
+		owner1 like 'TRUSTEES FOR MOORE'  or
+        (owner1 like 'UNIVERSITY%' OR owner2 like 'PENNSYLVANIA')
+        
 )
 
+select
+	count(*)::integer as count_block_groups
+from penn
+inner join census.blockgroups_2020 as blocks
+	on st_covers(penn.buff::geography, blocks.geog)
