34f3b2d8a65982b5206e6054f878df11d33cbdf5
nlebovits <nissim.lebovits@vanderbilt.edu>
2024-03-07 12:15:58 -0500

    update accessibility format and add z-scoring

--- query06.sql
+++ query06.sql
@@ -1,9 +1,13 @@
 WITH wheelchair_accessible_stops AS (
-  SELECT DISTINCT ON (geog) geog
+  SELECT geog, wheelchair_boarding
   FROM septa.bus_stops
   WHERE wheelchair_boarding = 1
+  UNION ALL
+  SELECT geog, wheelchair_boarding
+  FROM septa.bus_stops
+  WHERE wheelchair_boarding = 0
 ),
-unique_block_groups_near_stops AS (
+block_groups_near_stops AS (
   SELECT DISTINCT cbg.geoid
   FROM census.blockgroups_2020 AS cbg
   JOIN wheelchair_accessible_stops AS was
@@ -11,12 +15,22 @@ unique_block_groups_near_stops AS (
 ),
 populated_block_groups AS (
   SELECT 
-    ubgns.geoid, 
+    bgn.geoid, 
     SUM(cp.total) AS total_population
-  FROM unique_block_groups_near_stops AS ubgns
+  FROM block_groups_near_stops AS bgn
   JOIN census.population_2020 AS cp 
-  ON '1500000US' || ubgns.geoid = cp.geoid
-  GROUP BY ubgns.geoid
+  ON '1500000US' || bgn.geoid = cp.geoid
+  GROUP BY bgn.geoid
+),
+neighborhood_stop_counts AS (
+  SELECT
+    a.mapname,
+    SUM(CASE WHEN was.wheelchair_boarding = 1 THEN 1 ELSE 0 END) AS num_bus_stops_accessible,
+    SUM(CASE WHEN was.wheelchair_boarding = 0 THEN 1 ELSE 0 END) AS num_bus_stops_inaccessible
+  FROM azavea.neighborhoods AS a
+  JOIN wheelchair_accessible_stops AS was
+  ON ST_DWithin(a.geog, was.geog::geography, 100)
+  GROUP BY a.mapname
 ),
 neighborhood_populations AS (
   SELECT 
@@ -26,8 +40,24 @@ neighborhood_populations AS (
   JOIN populated_block_groups AS pbg
   ON ST_Intersects(a.geog, (SELECT geog FROM census.blockgroups_2020 WHERE geoid = pbg.geoid))
   GROUP BY a.mapname
-  ORDER BY 
-  total_population DESC
+),
+population_stats AS (
+  SELECT
+    AVG(total_population) AS avg_population,
+    STDDEV_POP(total_population) AS stddev_population
+  FROM neighborhood_populations
+),
+neighborhood_accessibility AS (
+  SELECT 
+    np.mapname AS neighborhood_name,
+    (np.total_population - ps.avg_population) / ps.stddev_population AS accessibility_metric,
+    nsc.num_bus_stops_accessible,
+    nsc.num_bus_stops_inaccessible
+  FROM neighborhood_populations np
+  CROSS JOIN population_stats ps
+  JOIN neighborhood_stop_counts nsc ON np.mapname = nsc.mapname
+  ORDER BY
+  accessibility_metric DESC
   LIMIT 5
 )
-SELECT * FROM neighborhood_populations;
+SELECT * FROM neighborhood_accessibility;
