77b0b310f532e7e6f413fa7d267b78a024990924
Dave Drennan <38985190+davedrenn@users.noreply.github.com>
2024-05-03 15:32:31 -0400

    fix linter error

--- query10.sql
+++ query10.sql
@@ -8,28 +8,36 @@ WITH dist AS (
         ) AS dist2ch
     FROM septa.rail_stops AS r
     GROUP BY r.stop_id, r.geog
+),
+
+stop_desc_calculation AS (
+    SELECT
+        rail.stop_id,
+        CONCAT(
+            dist.dist2ch,
+            ' mi to City Hall and ',
+            COUNT(bus.*),
+            ' bus stops within 1/4 mi'
+        ) AS stop_desc
+    FROM septa.rail_stops AS rail
+    LEFT JOIN septa.bus_stops AS bus
+        ON ST_DWITHIN(bus.geog, rail.geog, 402.336 * 3.28084)
+    LEFT JOIN dist
+        ON rail.stop_id = dist.stop_id
+    GROUP BY
+        rail.stop_id,
+        dist.dist2ch
 )
 
 SELECT
     rail.stop_id,
     rail.stop_name,
-    CONCAT(
-        dist.dist2ch,
-        ' mi to City Hall and ',
-        COUNT(bus.*),
-        ' bus stops within 1/4 mi'
-    ) AS stop_desc,
+    stop_desc_calculation.stop_desc,
     rail.stop_lon,
     rail.stop_lat
 FROM septa.rail_stops AS rail
-LEFT JOIN septa.bus_stops AS bus
-    ON ST_DWITHIN(bus.geog, rail.geog, 402.336 * 3.28084)
+LEFT JOIN stop_desc_calculation
+    ON rail.stop_id = stop_desc_calculation.stop_id
 LEFT JOIN dist
     ON rail.stop_id = dist.stop_id
-GROUP BY
-    rail.stop_id,
-    rail.stop_name,
-    rail.stop_lon,
-    rail.stop_lat,
-    dist.dist2ch
-ORDER BY dist.dist2ch ASC
+ORDER BY dist.dist2ch ASC;
