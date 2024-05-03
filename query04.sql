a30131e7acc1e03bd2a6e3fb83ec0fb81760d200
Dave Drennan <38985190+davedrenn@users.noreply.github.com>
2024-05-03 15:14:06 -0400

    python reference fix

--- query04.sql
+++ query04.sql
@@ -19,7 +19,7 @@ SELECT
     ROUND(ST_LENGTH(shapes.shape_geog)::numeric) AS shape_length
 FROM shapes
 INNER JOIN septa.bus_trips AS trips ON shapes.shape_id = trips.shape_id
-INNER JOIN septa.bus_routes AS routes ON routes.route_id = trips.route_id
+INNER JOIN septa.bus_routes AS routes ON trips.route_id = routes.route_id
 GROUP BY routes.route_short_name, trips.trip_headsign, shapes.shape_geog
 ORDER BY shape_length DESC
 LIMIT 2;
